a98980d40b3a739e979086cfba8426faa5502b28
Stevam Reis <stevamreis@gmail.com>
2025-02-14 00:38:31 -0300

    [0001] - criacao de env e ajustes no jwt

--- src/main/java/br/com/onetalk/controller/AuthController.java
+++ src/main/java/br/com/onetalk/controller/AuthController.java
@@ -1,7 +1,6 @@
 package br.com.onetalk.controller;
 
 import br.com.onetalk.controller.request.UserRequest;
-import br.com.onetalk.controller.response.JwtResponse;
 import br.com.onetalk.model.Roles;
 import br.com.onetalk.service.AuthService;
 import jakarta.annotation.security.PermitAll;
@@ -9,6 +8,8 @@ import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
+
+import java.util.Map;
 
 
 @Path("/auth")
@@ -34,48 +35,23 @@ public class AuthController {
     @POST
     @PermitAll
     @Path("/login")
-    public Response login(UserRequest userRequest){
+    public Response login(UserRequest userRequest) {
         String token = authService.authenticate(userRequest.getEmail(), userRequest.getPassword());
-        NewCookie cookie = new NewCookie.Builder("access_token")
-                .value(token)
-                .path("/")
-                .domain(null)
-                .comment("JWT Token")
-                .maxAge(3600)
-                .secure(true)
-                .httpOnly(true)
-                .sameSite(NewCookie.SameSite.NONE)
-                .build();
-
-        return Response.ok().cookie(cookie).entity(new JwtResponse(token)).build();
+        return Response.ok(Map.of("token", token)).build();
     }
 
     @GET
     @Path("/authenticated")
     @RolesAllowed(Roles.USER_ROLE)
-    public Response getUser(@Context HttpHeaders headers, @Context SecurityContext securityContext) {
+    public Response isAuthenticated(@Context SecurityContext securityContext) {
         String username = securityContext.getUserPrincipal().getName();
-        Cookie cookie = headers.getCookies().get("access_token");
-        if (cookie == null) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        }
-        return Response.ok("{\"user\": \"" + username + "\"}").build();
+        return Response.ok(Map.of("user", username)).build();
     }
 
     @POST
     @Path("/logout")
-    @RolesAllowed(Roles.USER_ROLE)
     public Response logout() {
-        NewCookie expiredCookie = new NewCookie.Builder("access_token")
-                .value("")
-                .path("/")
-                .domain(null)
-                .comment("JWT Token")
-                .maxAge(0)
-                .secure(true)
-                .httpOnly(true)
-                .build();
-        return Response.ok().cookie(expiredCookie).build();
+        return Response.ok("Logout realizado com sucesso").build();
     }
 
 }
--- src/main/java/br/com/onetalk/service/AuthService.java
+++ src/main/java/br/com/onetalk/service/AuthService.java
@@ -8,11 +8,9 @@ import io.smallrye.jwt.build.Jwt;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
-import lombok.SneakyThrows;
 import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
-import javax.security.sasl.AuthenticationException;
 import java.util.Collections;
 import java.util.HashSet;
 
@@ -27,11 +25,9 @@ public class AuthService {
         this.userRepository = userRepository;
     }
 
-
-    @SneakyThrows
     public void register(String name, String email, String password) {
         if (userRepository.findByEmail(email) != null) {
-            throw new AuthenticationException("Email Already exist");
+            throw new WebApplicationException("Email Already exist", Response.Status.NOT_FOUND);
         }
         User user = new User(name, email, BcryptUtil.bcryptHash(password), new HashSet<>(Collections.singletonList(Roles.USER_ROLE)));
         userRepository.persist(user);
@@ -40,12 +36,24 @@ public class AuthService {
     public String authenticate(String email, String password) {
         User user = userRepository.findByEmail(email);
         if (user == null || !BcryptUtil.matches(password, user.getPasswordHash())) {
-            throw new WebApplicationException("Invalid credencials", Response.Status.UNAUTHORIZED);
+            throw new WebApplicationException("Invalid credentials", Response.Status.UNAUTHORIZED);
         }
-        return Jwt.issuer("onetalk-api").upn(user.getEmail())
+
+        String accessToken = Jwt.issuer("onetalk-api")
+                .upn(user.getEmail())
                 .claim(Claims.email.name(), user.getEmail())
                 .claim(Claims.nickname.name(), user.getEmail())
-                .groups(user.getRoles()).sign();
+                .groups(user.getRoles())
+                .expiresIn(900) // 15 minutos
+                .sign();
+
+        String refreshToken = Jwt.issuer("onetalk-api")
+                .upn(user.getEmail())
+                .claim(Claims.email.name(), user.getEmail())
+                .expiresIn(86400) // 1 dia
+                .sign();
+
+        return accessToken + ":" + refreshToken;
     }
 
 }
